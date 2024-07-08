c75701c585b7c46fa92f4a1df9f0df4b7165f22c
Joe Roddy <mrjoeroddy@gmail.com>
2024-07-08 11:54:21 -0400

    update chart ui for small screens

--- src/components/PieCharts.tsx
+++ src/components/PieCharts.tsx
@@ -1,11 +1,7 @@
 import { ExpenseCategory, IncomeCategory } from '@/types';
 import { PieChart } from 'react-minimal-pie-chart';
 
-export function PieWithLegend({
-  data,
-}: {
-  data: Record<ExpenseCategory, number> | Record<IncomeCategory, number>;
-}) {
+export function PieWithLegend({ data }: { data: Record<ExpenseCategory, number> | Record<IncomeCategory, number> }) {
   const arrayData = Object.entries(data).map(([category, amount]) => ({
     category,
     value: amount,
@@ -13,16 +9,12 @@ export function PieWithLegend({
   }));
 
   return (
-    <div className="h-48 grid items-center gap-3 grid-cols-3">
-      <div className="col-span-1"></div>
+    <div className="h-48 flex justify-center items-center gap-3 grid-cols-3">
       <PieChart data={arrayData} />
       <div className="flex flex-col">
         {arrayData.map((c) => (
           <div key={c.category} className="flex gap-1 items-center">
-            <div
-              className="size-4 rounded-full"
-              style={{ backgroundColor: c.color }}
-            />
+            <div className="size-4 rounded-full" style={{ backgroundColor: c.color }} />
             {c.category}
           </div>
         ))}
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,31 +1,13 @@
 import { ExpenseCategory, IncomeCategory } from '@/types';
 
-const INCOME_CATEGORIES: IncomeCategory[] = [
-  'Salary',
-  'Freelance',
-  'Side Project Income',
-  'Other',
-];
+const INCOME_CATEGORIES: IncomeCategory[] = ['Salary', 'Freelance', 'Side Project Income', 'Other'];
 
-const EXPENSE_CATEGORIES: ExpenseCategory[] = [
-  'Food',
-  'Transportation',
-  'Housing',
-  'Entertainment',
-  'Other',
-];
+const EXPENSE_CATEGORIES: ExpenseCategory[] = ['Food', 'Transportation', 'Housing', 'Entertainment', 'Other'];
 
 export type ExpenseFilter = 'all' | 'none' | ExpenseCategory;
-export function ExpenseSelect({
-  onSelect,
-}: {
-  onSelect?: (filter: ExpenseFilter) => void;
-}) {
+export function ExpenseSelect({ onSelect }: { onSelect?: (filter: ExpenseFilter) => void }) {
   return (
-    <select
-      className="absolute top-5 right-5"
-      onChange={(e) => onSelect?.(e.target.value as ExpenseFilter)}
-    >
+    <select className="absolute top-5 right-5 w-20" onChange={(e) => onSelect?.(e.target.value as ExpenseFilter)}>
       <option value="all">View All</option>
       {EXPENSE_CATEGORIES.map((category) => (
         <option key={category} value={category}>
@@ -38,16 +20,9 @@ export function ExpenseSelect({
 }
 
 export type IncomeFilter = 'all' | 'none' | IncomeCategory;
-export function IncomeSelect({
-  onSelect,
-}: {
-  onSelect?: (filter: IncomeFilter) => void;
-}) {
+export function IncomeSelect({ onSelect }: { onSelect?: (filter: IncomeFilter) => void }) {
   return (
-    <select
-      className="absolute top-5 right-5"
-      onChange={(e) => onSelect?.(e.target.value as IncomeFilter)}
-    >
+    <select className="absolute top-5 right-5 w-20" onChange={(e) => onSelect?.(e.target.value as IncomeFilter)}>
       <option value="all">View All</option>
       {INCOME_CATEGORIES.map((category) => (
         <option key={category} value={category}>
